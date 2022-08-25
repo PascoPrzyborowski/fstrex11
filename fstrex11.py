ad770502dc9718d3031c3b7213c0b1d4aa6409fc
PascoPrzyborowski <unity01@gmx.net>
2022-08-25 17:02:55 +0200

    fstrex11

---
+++ fstrex11.py
@@ -0,0 +1,16 @@
+
+
+
+#Task1
+
+
+import re
+
+
+def replace_it(text):
+    return text.replace(" dog ", " cat ")
+
+
+
+print(replace_it("A dogmatic dog buys dogecoin to become rich and buy hotdogs every day."))
+
